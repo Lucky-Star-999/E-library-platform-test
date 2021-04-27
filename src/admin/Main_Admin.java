5735230d14fab269e0978a0164f2d1eaaddf5bbc
Lucky-Star-999 <64720426+Lucky-Star-999@users.noreply.github.com>
2021-04-27 21:10:04 +0700

    Initialize files

---
+++ src/admin/Main_Admin.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package admin;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Main_Admin {
+    public static void main(String[] args){
+        Frame01_IntroAdmin frame1_introadmin = new Frame01_IntroAdmin();
+        frame1_introadmin.setVisible(true);
+    }
+}
